2ee9d37c5854da91bbe595f621c1df63300e0f8a
beatcracker <beatcracker@users.noreply.github.com>
2015-05-26 23:42:17 +0300

    Fixed module import, added Verbose messages
    This function will only try to import well-formed modules. A
    "well-formed" module is a module that is stored in a directory at as the
    same name as the base name of at least one file in the module directory.
    
    More info: https://msdn.microsoft.com/en-us/library/dd878350.aspx

--- Import-Component.ps1
+++ Import-Component.ps1
@@ -1,6 +1,6 @@
 <#
 .Synopsis
-	Bulk-import from folder any component, supported by PowerShell (script, module, source code, .Net assembly). 
+	Bulk-import from folder any component, supported by PowerShell (script, module, source code, .Net assembly).
 
 .Description
 	Bulk-import from folder any component, supported by PowerShell. Supported components:
@@ -16,7 +16,7 @@
 	This parameter is required.
 
 	A string representing the path, where components located. Wildcards are not supported.
-	This is the folder, where you could keep your functions in .ps1 scripts, modules in .psm1 files,
+	This is the folder, where you could keep your functions in .ps1 scripts, modules,
 	C# (VB.Net, JavaScript) code in .cs, .vb, .js files and .Net assemblies (.dll).
 
 .Parameter Recurse
@@ -38,7 +38,18 @@
 		Script (.ps1) - imported using Dot-Sourcing.
 
 	Psm
-		Module (.psm1) - imported using Import-Module cmdlet
+		Module - imported using Import-Module cmdlet
+
+		This function will only try to import well-formed modules. A "well-formed" module is a module
+		that is stored in a directory that has the same name as the base name of at least one file in
+		the module directory. If a module is not well-formed, Windows PowerShell does not recognize it
+		as a module.
+
+		The "base name" of a file is the name without the file name extension. In a well-formed module,
+		the name of the directory that contains the module files must match the base name of at least
+		one file in the module.
+
+		More info: https://msdn.microsoft.com/en-us/library/dd878350.aspx
 
 	Cs
 		Source code (.cs) - imported using Add-Type cmdlet
@@ -68,15 +79,15 @@
 
 		Description
 		-----------
-		Import all supported components (.ps1, .psm1, .cs, .vb, .js, .dll) from folder 'C:\PsLib'.
+		Import all supported components (.ps1, modules, .cs, .vb, .js, .dll) from folder 'C:\PsLib'.
 		Note, that to be able to import .ps1 scripts into the current scope, this function is dot-sourced.
 
 .Example
-	'C:\PsLib', 'C:\MyLib' | . Import-Component 
+	'C:\PsLib', 'C:\MyLib' | . Import-Component
 
 		Description
 		-----------
-		Import all supported components (.ps1, .psm1, .cs, .vb, .js, .dll) from folders 'C:\PsLib' and 'C:\MyLib'.
+		Import all supported components (.ps1, modules, .cs, .vb, .js, .dll) from folders 'C:\PsLib' and 'C:\MyLib'.
 		Note, that to be able to import .ps1 scripts into the current scope, this function is dot-sourced.
 
 .Example
@@ -84,7 +95,7 @@
 
 		Description
 		-----------
-		Import all supported components (.ps1, .psm1, .cs, .vb, .js, .dll), recurse into subdirectories.
+		Import all supported components (.ps1, modules, .cs, .vb, .js, .dll), recurse into subdirectories.
 		Note, that to be able to import .ps1 scripts into the current scope, this function is dot-sourced.
 
 .Example
@@ -92,7 +103,7 @@
 
 		Description
 		-----------
-		Import all supported components (.ps1, .psm1, .cs, .vb, .js, .dll), recurse into subdirectories.
+		Import all supported components (.ps1, modules, .cs, .vb, .js, .dll), recurse into subdirectories.
 		Include only files with names without extension that match wildcard 'MyScript*'.
 		Note, that to be able to import .ps1 scripts into the current scope, this function is dot-sourced.
 
@@ -101,7 +112,7 @@
 
 		Description
 		-----------
-		Import all supported components (.ps1, .psm1, .cs, .vb, .js, .dll), recurse into subdirectories.
+		Import all supported components (.ps1, modules, .cs, .vb, .js, .dll), recurse into subdirectories.
 		Include only files with names without extension that match wildcard 'MyScript*'.
 		Exclude files with names without extension that match '*_backup*' wildcard.
 		Note, that to be able to import .ps1 scripts into the current scope, this function is dot-sourced.
@@ -111,7 +122,7 @@
 
 		Description
 		-----------
-		Import all supported components (.ps1, .psm1, .cs, .vb, .js, .dll), recurse into subdirectories.
+		Import all supported components (.ps1, modules, .cs, .vb, .js, .dll), recurse into subdirectories.
 		Include only files with names without extension that match wildcards 'MyScript*' and '*MyLib*'.
 		Exclude files with names without extension that match '*_backup*' and '*_old*' wildcards.
 		Note, that to be able to import .ps1 scripts into the current scope, this function is dot-sourced.
@@ -121,14 +132,14 @@
 
 		Description
 		-----------
-		Import Powershell modules only (.psm1).
+		Import Powershell modules only.
 
 .Example
-	Import-Component 'C:\PsLib' -Type Psm,Cs,Asm
+	Import-Component 'C:\PsLib' -Type Ps,Cs,Asm
 
 		Description
 		-----------
-		Import Powershell modules (.psm1), C# source code (.cs), .Net assemblies (.dll).
+		Import Powershell modules, C# source code (.cs), .Net assemblies (.dll).
 #>
 function Import-Component
 {
@@ -154,19 +165,28 @@ function Import-Component
 
 	Begin
 	{
-
 		$MyCommand =  (Get-PSCallStack)[1].Position.Text
 		$MyCommandName = $MyInvocation.MyCommand.Name
 		$Split = $MyCommand -split "($MyCommandName)"
 		$IsDotSourced = $Split[0] -match '\.\s*$'
+		Write-Verbose "Function dot-sourced: $IsDotSourced"
 
+		# Define scriptblocks that will import verious file types
 		$DotSource = {. $_.FullName}
-		$ImportModule = {Import-Module $_.FullName -ErrorAction SilentlyContinue}
 		$AddType = {Add-Type -LiteralPath $_.FullName -ErrorAction SilentlyContinue}
+		$ImportModule = {
+			if(Get-ChildItem -Path ($_.FullName + '\*') -Filter ($_.Basename + '.*'))
+			{
+				Write-Verbose "Folder '$($_.Basename)' looks like well-formed module"
+				# https://msdn.microsoft.com/en-us/library/dd878350.aspx
+				Import-Module $_.FullName -ErrorAction SilentlyContinue
+			}
+		}
 
+		# Define extensions for file types and assign import commands
 		$FileType = @{
 			Ps = @{Extension = '.ps1' ; Command = $DotSource}
-			Psm = @{Extension = '.psm1' ; Command = $ImportModule}
+			Psm = @{Extension = [string]::Empty ; Command = $ImportModule}
 			Cs = @{Extension = '.cs' ; Command = $AddType}
 			Vb = @{Extension = '.vb' ; Command = $AddType}
 			Js = @{Extension = '.js' ; Command = $AddType}
@@ -178,10 +198,12 @@ function Import-Component
 	{
 		if($Type)
 		{
+			Write-Verbose "Trying to import file types $($Type -join ',')"
 			[array]$FileTypeToImport =  $Type | Sort-Object -Unique | ForEach-Object {$FileType.$_}
 		}
 		else
 		{
+			Write-Verbose 'Trying to import all supported file types'
 			[array]$FileTypeToImport = $FileType.GetEnumerator() | ForEach-Object {$_.Value}
 		}
 
@@ -190,10 +212,12 @@ function Import-Component
 			Write-Warning "To import .PS1 scripts this function itself has to be dot-sourced! Example: $($Split[$Split.IndexOf($MyCommandName)] = ". $MyCommandName" ; $Split -join '')"
 		}
 
+		# Filter function to process Include\Exclude parameters
 		filter Skip-File {
 			if
 			(
-				!($_.PSIsContainer) -and 
+				# No file extension and directory = module, else - file
+				((!$currFT.Extension -and $_.PSIsContainer) -or ($currFT.Extension -and !$_.PSIsContainer)) -and
 				$($tmp = $_.BaseName ; $Exclude | ForEach-Object {if($tmp -notlike $_){$true}}) -and
 				$($tmp = $_.BaseName ; $Include | ForEach-Object {if($tmp -like $_){$true}})
 			)
@@ -204,18 +228,23 @@ function Import-Component
 
 		$FileTypeToImport |
 			ForEach-Object {
-				$Private:currFT = $_
+				# We need to pass current file type to the filter function later
+				Set-Variable -Name currFT -Value $_ -Option AllScope
+				Write-Verbose "Searching path '$Path' for file type '$(('*' + $_.Extension))'"
 				Get-ChildItem -LiteralPath $Path -Filter ('*' + $_.Extension) -Recurse:$Recurse |
 					Skip-File |
 						ForEach-Object {
 							Try
 							{
-								. $Private:currFT.Command
+								Write-Verbose "Trying to import: $_"
+								. $currFT.Command
 								$Success = $true
 								$ErrMsg = $null
+								Write-Verbose 'Success'
 							}
 							Catch
 							{
+								Write-Verbose 'Failed'
 								$Success = $false
 								$ErrMsg = $_
 							}
@@ -227,6 +256,7 @@ function Import-Component
 								Error = $ErrMsg
 							}
 
+							Write-Verbose "Writing import status for '$_' to pipeline"
 							New-Object PSObject -Property $ret | Select-Object Name, Path, Loaded, Error
 						}
 			}
